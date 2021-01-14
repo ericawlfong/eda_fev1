36d30f8b276e8a3d7eed12bd546675c630e234f4
Erica <ericawlfong@gmail.com>
2021-01-14 16:55:52 +0000

    Update eda.R
    3c, 4a done

--- eda.R
+++ eda.R
@@ -20,22 +20,30 @@ summarise(fev1, cor = cor(fev1$age,fev1$FEV1))
 
 age_fev1_plot <- ggplot(data = fev1, aes(x = fev1$age,
                                          y = fev1$FEV1)) +
-  geom_point() +
+  geom_point() + geom_smooth() +
   xlab("Age") + ylab("FEV1")
 print(age_fev1_plot)
 
 # Answer: <discuss linear model assumption>
 
 # E3c. outliers
+fev1_outliers <- fev1 %>%
+  group_by(id) %>%
+  summarise(corr = cor(age,FEV1)) %>%
+  filter(corr < 0) %>%
+  view()
 
 # hint: do groupwise calculation of r and then filter/subset 
 
 ## Answer: <why would r < 0?>
 
-# 4a. how many?
+# 4a. how many? 1
 
 # count how many times each id appears
-counts <- ...
+counts <- fev1 %>%
+  group_by(id) %>%
+  summarise(n())
+
 # hint: dplyr has a function to do this, or you can use data.table, aggregate, 
 # or even a loop if you really needed to
 
